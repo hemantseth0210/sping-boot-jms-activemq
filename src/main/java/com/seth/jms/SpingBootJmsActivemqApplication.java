bd1f992f41d53c5575a27f4e609e1e6af191e6f6
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2020-12-02 00:21:01 -0500

    First Commit

---
+++ src/main/java/com/seth/jms/SpingBootJmsActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.seth.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootJmsActivemqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootJmsActivemqApplication.class, args);
+	}
+
+}
